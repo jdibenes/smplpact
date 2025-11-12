4d259fcb24d856c13ef33998d8620b29333b048b
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-11-11 23:49:23 -0500

    Update smpl_paint_demo_probe.py

--- smpl_paint_demo_probe.py
+++ smpl_paint_demo_probe.py
@@ -16,15 +16,14 @@ import time
 import json
 import cv2
 import numpy as np
-import torch
 import trimesh
 import smplpact
 
 
 class demo:
     def run(self):
-        # Settings
-        self._device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        # Settings ------------------------------------------------------------
+        self._device = 'cuda'
 
         self._smpl_dataset_path = 'C:/Users/jcds/Desktop/Oct_22_stevens/output_pairs/'
         self._smpl_model_path = './data/smpl/SMPL_NEUTRAL.pkl'
@@ -42,17 +41,22 @@ class demo:
         self._camera_use_plane = True
 
         self._cursor_radius = 0.015
-        self._cursor_height = 0.04
 
         self._brush_size = 0.02
         self._brush_color_center = np.array([255, 0, 0, 255], dtype=np.uint8)
-        self._brush_color_edge = np.array([255, 255, 0, 255], dtype=np.uint8)
-        self._brush_hardness = 0.33
+
+        self._probe_image_radius = 7
+        self._probe_image_color = (255, 0, 255)
+        self._probe_image_thickness = 3
 
         self._fps_period = 2.0
-        # End Settings
+        # End Settings --------------------------------------------------------
 
-        print(f'Using device: {self._device}')
+        # Load RealSense parameters
+        self._realsense_K = np.array([[1361.8736572265625, 0, 963.2017211914062],[0, 1361.03076171875, 552.251220703125],[0, 0, 1]], dtype=np.float32)
+        
+        # Load SMPL texture
+        self._texture_array = smplpact.texture_load_image(self._smpl_texture_path, load_alpha=self._smpl_texture_load_alpha)
 
         # Create offscreen renderer
         fxy = smplpact.geometry_fov_to_f(self._camera_fov_vertical, self._viewport_height)
@@ -62,56 +66,53 @@ class demo:
         cfg_camera = smplpact.renderer_create_settings_camera(fxy, fxy, self._viewport_width // 2, self._viewport_height // 2)
         cfg_camera_transform = smplpact.renderer_create_settings_camera_transform()
         cfg_lamp = smplpact.renderer_create_settings_lamp()
-        
-        self._offscreen_renderer = smplpact.renderer(cfg_offscreen, cfg_scene, cfg_camera, cfg_camera_transform, cfg_lamp)
-        # Load SMPL texture
-        self._texture_array = smplpact.texture_load_image(self._smpl_texture_path, load_alpha=self._smpl_texture_load_alpha)
+        cfg_smpl_model = smplpact.renderer_create_settings_smpl_model(self._smpl_model_path, 10, self._device)
+        cfg_smpl_uv = smplpact.renderer_create_settings_smpl_uv(self._smpl_uv_path, self._texture_array.shape)
+        cfg_smpl_filter_bb = smplpact.renderer_create_settings_smpl_filter_bounding_box([670, 0, 1400, 1080], [smplpact.smpl_joints_openpose.MidHip, smplpact.smpl_joints_openpose.LHip, smplpact.smpl_joints_openpose.RHip], [0.4, 0.3, 0.3], 0.59)
+        cfg_smpl_filter_ff = smplpact.renderer_create_settings_smpl_filter_forward_face()
+        cfg_smpl_filter_ef = smplpact.renderer_create_settings_smpl_filter_exponential_single([0.25, 0.25, 0.25, 0.9])
+        cfg_smpl_filter_ow = smplpact.renderer_create_settings_smpl_filter_fixed_joints()
 
-        # Load SMPL model
-        self._offscreen_renderer.smpl_load_model(self._smpl_model_path, 10, self._device)
-        self._offscreen_renderer.smpl_load_uv(self._smpl_uv_path, self._texture_array.shape)
-
-        # Load realsense intrinsics
-        self._K = np.array([[1361.8736572265625, 0, 963.2017211914062],[0, 1361.03076171875, 552.251220703125],[0, 0, 1]], dtype=np.float32)
+        self._offscreen_renderer = smplpact.renderer_context(cfg_offscreen, cfg_scene, cfg_camera, cfg_camera_transform, cfg_lamp, cfg_smpl_model, cfg_smpl_uv, cfg_smpl_filter_bb, cfg_smpl_filter_ff, cfg_smpl_filter_ef, cfg_smpl_filter_ow)
         
         # Create UI elements
-        self._cursor_mesh = trimesh.creation.cone(radius=self._cursor_radius, height=self._cursor_height)
+        self._cursor_mesh = trimesh.creation.icosphere(radius=self._cursor_radius)
         self._cursor_pose = np.eye(4, dtype=np.float32)
 
+        # Load dataset
         files, folders = smplpact.scan_path(self._smpl_dataset_path, folders_sort=True, folders_key=lambda x : int(x.split('_')[-1]))
         index = 0
 
-        # Configure filters
-        self._offscreen_renderer.smpl_filter_reset()
-        self._offscreen_renderer.smpl_filter_set_bounding_box([670, 0, 1400, 1080], [smplpact.smpl_joints_openpose.MidHip, smplpact.smpl_joints_openpose.LHip, smplpact.smpl_joints_openpose.RHip], np.array([0.4, 0.3, 0.3], dtype=np.float32), 0.59)
-        self._offscreen_renderer.smpl_filter_set_forward_face(None)
-        self._offscreen_renderer.smpl_filter_set_exponential_single([0.25, 0.25, 0.25, 0.9])
+        # Print configuration
+        print(f'Using device: {self._device}')
+        print(f'SMPL texture shape: {self._texture_array.shape}')
 
         # Run inference and painting
         start = time.perf_counter()
         count = 0
         while (True):
-            self._smpl_test_message_path = os.path.join(folders[index], 'pose.json')
-            self._probe_test_message_path = os.path.join(folders[index], 'aruco.json')
-            self._test_image_path = os.path.join(folders[index], 'realsense_image.jpg')
-            print(folders[index])
+            # Get paths
+            self._smpl_message_path = os.path.join(folders[index], 'pose.json')
+            self._probe_message_path = os.path.join(folders[index], 'aruco.json')
+            self._realsense_image_path = os.path.join(folders[index], 'realsense_image.jpg')
+
             index = (index + 1) % len(folders)
 
-            # Load test CameraHMR message
-            with open(self._smpl_test_message_path, 'rt') as json_file:
-                self._test_camerahmr_message = json.load(json_file)
+            # Load pose message
+            with open(self._smpl_message_path, 'rt') as json_file:
+                self._pose_message = json.load(json_file)
 
-            # Load test probe message
-            with open(self._probe_test_message_path, 'rt') as probe_file:
+            # Load probe message
+            with open(self._probe_message_path, 'rt') as probe_file:
                 self._probe_message = json.load(probe_file)
 
-            # Load test image
-            self._test_image = cv2.imread(self._test_image_path, cv2.IMREAD_COLOR_BGR)
-            #self._test_image = np.asarray(Image.open(self._test_image_path))[:,:,::-1]
-            if (self._test_image is None):
+            # Load image
+            self._realsense_image = cv2.imread(self._realsense_image_path, cv2.IMREAD_COLOR_BGR)
+            if (self._realsense_image is None):
                 continue
 
-            status = self._loop()
+            with self._offscreen_renderer:
+                status = self._paint()
 
             count += 1
             end = time.perf_counter()
@@ -124,18 +125,14 @@ class demo:
             if (not status):
                 break
 
-    def _loop(self):
+    def _paint(self):
         # SMPL params to mesh
-        smpl_params, self._K_smpl = self.smpl_unpack_cliff(self._test_camerahmr_message)
-
-        smpl_ok, smpl_result = self._offscreen_renderer.smpl_get_mesh(smpl_params, self._K_smpl.T, self._K.T)
-        
+        smpl_params, smpl_K = self._offscreen_renderer.smpl_unpack(self._pose_message)
+        smpl_ok, smpl_result = self._offscreen_renderer.smpl_get_mesh(smpl_params, smpl_K.T, self._realsense_K.T)
         smpl_vertices = smpl_result.vertices[0]
         smpl_joints = smpl_result.joints[0]
         smpl_faces = smpl_result.faces
-        # ^ FILTER for 0 meshes ^
-
-        smpl_mesh = smplpact.mesh_create(smpl_vertices, smpl_faces, visual=None)
+        smpl_mesh = smplpact.mesh_create(smpl_vertices, smpl_faces)
 
         # Compute pose to set mesh upright
         # Poses convert from object to world
@@ -146,22 +143,15 @@ class demo:
 
         # Set probe position
         probe_position = np.array([[self._probe_message['x'], self._probe_message['y'], self._probe_message['z']]], dtype=np.float32)
-        #probe_position *= 0.94 # heuristic
-        self._cursor_pose[3:4, :3] = smplpact.math_transform_points(probe_position, smpl_mesh_pose.T, inverse=False)
         
+        self._cursor_pose[:3, 3:4] = smplpact.math_transform_points(probe_position, smpl_mesh_pose.T, inverse=False).T
+
         # Add cursor to the main scene
-        cursor_pose = self._cursor_pose.T
-        cursor_mesh_id = self._offscreen_renderer.mesh_add_user('ui', 'cursor', self._cursor_mesh, cursor_pose)
+        cursor_mesh_id = self._offscreen_renderer.mesh_add_user('ui', 'cursor', self._cursor_mesh, self._cursor_pose)
 
-        # Find closest point to mesh
-        cursor_anchor = self._offscreen_renderer.mesh_operation_closest(smpl_mesh_id, self._cursor_pose[3:4, :3])
-
-        # Paint SMPL mesh
-        # Paint circular gradient at camera mesh intersection/closest
+        # Find closest point to mesh and paint
+        cursor_anchor = self._offscreen_renderer.mesh_operation_closest(smpl_mesh_id, self._cursor_pose[:3, 3:4].T)
         if (cursor_anchor.point is not None):
-            # Gradient option
-            #self._offscreen_renderer.smpl_paint_brush_gradient(smpl_mesh_id, cursor_anchor, self._brush_size, self._brush_color_center, self._brush_color_edge, self._brush_hardness, fill_test=0.25)
-            # Solid color option
             self._offscreen_renderer.smpl_paint_brush_solid(smpl_mesh_id, cursor_anchor, self._brush_size, self._brush_color_center, fill_test=0.25)
         
         # Finalize SMPL painting
@@ -180,12 +170,13 @@ class demo:
         # Show rendered image
         cv2.imshow('SMPL Paint Demo', cv2.cvtColor(color, cv2.COLOR_RGB2BGR))
 
-        joints_image = (smpl_vertices/smpl_vertices[:,2:3]) @ self._K.T
-        for i in range(0, joints_image.shape[0]):
-            center = (int(joints_image[i, 0]), int(joints_image[i, 1]))
-            cv2.circle(self._test_image, center, 5, (0, 255, 0), -1)
+        # Draw probe
+        probe_image = (probe_position / probe_position[:, 2:3]) @ self._realsense_K.T
+        center = (int(probe_image[0, 0]), int(probe_image[0, 1]))
+        cv2.circle(self._realsense_image, center, self._probe_image_radius, self._probe_image_color, self._probe_image_thickness)
 
-        cv2.imshow('RealSense', self._test_image)
+        # Show image
+        cv2.imshow('RealSense', self._realsense_image)
 
         # Process keyboard input
         key = cv2.waitKey(1) & 0xFF
@@ -219,31 +210,6 @@ class demo:
             return False
         
         return True
-
-    def smpl_unpack_camerahmr(self, msg):
-        person_list = msg['persons']
-        global_orient = torch.tensor([person['smpl_params']['global_orient'] for person in person_list], dtype=torch.float32, device=self._device)
-        body_pose = torch.tensor([person['smpl_params']['body_pose'] for person in person_list], dtype=torch.float32, device=self._device)
-        betas = torch.tensor([person['smpl_params']['betas'] for person in person_list], dtype=torch.float32, device=self._device)
-        camera_translation = torch.tensor([person['camera_translation'] for person in person_list], dtype=torch.float32, device=self._device)
-        smpl_params = { 'global_orient' : global_orient, 'body_pose' : body_pose, 'betas' : betas, 'transl' : camera_translation }
-        f = person_list[0]['focal_length']
-        w, h = msg['image_size']
-        K_smpl = np.array([[f, 0, w / 2], [0, f, h / 2], [0, 0, 1]], dtype=np.float32) 
-        return smpl_params, K_smpl
-    
-    def smpl_unpack_cliff(self, msg):
-        person_list = msg['persons']
-        smpl_pose = torch.tensor([person['smpl_pose'] for person in person_list], dtype=torch.float32, device=self._device)
-        global_orient = smpl_pose[:, 0:1, :, :]
-        body_pose = smpl_pose[:, 1:, :, :]
-        betas = torch.tensor([person['smpl_shape'] for person in person_list], dtype=torch.float32, device=self._device)
-        camera_translation = torch.tensor([person['camera_params'] for person in person_list], dtype=torch.float32, device=self._device)
-        smpl_params = { 'global_orient' : global_orient, 'body_pose' : body_pose, 'betas' : betas, 'transl' : camera_translation }
-        f = person_list[0]['focal_length']
-        w, h = msg['image_size']
-        K_smpl = np.array([[f, 0, w / 2], [0, f, h / 2], [0, 0, 1]], dtype=np.float32) 
-        return smpl_params, K_smpl
 
 
 def main():
